8824d45103e5d52b08929f363706b469cd5d8249
emcdona1 <emcdona1@neiu.edu>
2019-07-06 11:29:32 -0500

    Added docstrings to methods

--- API work/teacher_list.py
+++ API work/teacher_list.py
@@ -9,7 +9,8 @@ assert (len(sys.argv) > 1), "Output file name required"
 
 
 def get_employees():
-    url = "https://api.teachworks.com/v1/employees"
+    """Sends one API request to Teachworks, which returns a JSON of the first 50 active employees."""
+	url = "https://api.teachworks.com/v1/employees"
     querystring = {"per_page": "50", "status": "active"}
     headers = {
         'Authorization': "<<add token here>>",
@@ -19,17 +20,16 @@ def get_employees():
         "GET", url, headers=headers, params=querystring).json()
     return employees
 
-
 def filter_out_staff(employees):
+	"""Removes all non-teachers and test objects from the employees object."""
     for idx, e in enumerate(employees):
         if(e['employee_type'] != 'Teacher'):
             del employees[idx]
         if('Test' in e['first_name']):
             del employees[idx]
 
-
 def write_file(teachers):
-    "This function writes to a TSV file, formatted: [teacher id] \\t [first name] [last name] \\n"
+	"""Takes in the employee JSON and outputs a file of teacher ID's and names (TSV by default), formatted: [teacher id] \\t [first name] [last name] \\n"""
     f = open(sys.argv[1], mode='w')
     max = len(teachers) - 1
     for idx, t in enumerate(teachers):
@@ -39,7 +39,6 @@ def write_file(teachers):
             output_line += '\n'
         f.write(output_line)
     f.close()
-
 
 employees = get_employees()
 filter_out_staff(employees)
